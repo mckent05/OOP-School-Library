5fe2c08808d81ed5d954b8bd61f636004a18754f
MCKENT05 <atemitope95@gmail.com>
2022-01-27 21:30:07 +0100

    added basic UI to app

--- student.rb
+++ student.rb
@@ -4,13 +4,13 @@ class Student < Person
   attr_reader :classroom
 
   def initialize(classroom, age, name = 'unknown', parent_permission: true)
-    super(age, name, parent_permission)
+    super(age, name, parent_permission: parent_permission)
     @classroom = classroom
   end
 
   def classroom=(classroom)
     @classroom = classroom
-    classroom.student.push(self) unless classroom.student.includes?(self)
+    classroom.student.push(self) unless classroom.student.include?(self)
   end
 
   def play_hooky
